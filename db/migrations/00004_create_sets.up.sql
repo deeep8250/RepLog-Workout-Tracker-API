9943aaa44c8fa125328c1dc6180abe001250b34b
deeep8250 <deepmondal825005@gmail.com>
2026-03-06 07:24:30 +0530

    Week 1 Day 5: exercises seed data

--- db/migrations/00004_create_sets.up.sql
+++ db/migrations/00004_create_sets.up.sql
@@ -3,7 +3,8 @@ create table if not exists sets(
     workout_id int references workouts(id) on delete cascade,
     exercise_id int references exercises(id) on delete cascade,
     reps int not null,
-    weight_kg int not null
+    duration int ,
+    weight_kg varchar(255) not null
 );
 
 create index if not exists idx_sets_workout_id on sets(workout_id);
---
+++ db/migrations/seeds/exercises.up.sql
@@ -0,0 +1,14 @@
+insert into exercises (name,muscle_group,created_by) values 
+('bird dog','core',null),
+('cat cow','spine',null),
+('cobra pose','lower back',null),
+
+('jumping jack','full body', null),
+('high knee','leg',null),
+('squats','thighs',null),
+
+('push ups', 'chest',null),
+('sit ups','abs',null),
+('pull ups','back',null),
+
+('meditation','mind',null);
