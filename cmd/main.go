1626ad4fd4eb2d1e02c1a78c0f4d24fd970234ed
deeep8250 <deepmondal825005@gmail.com>
2026-03-16 20:37:00 +0530

    fix the  service failed issue

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"replog/internal/handlers"
+	"replog/internal/repository"
+	"replog/internal/services"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -15,12 +22,44 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
+	DSN := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	var db *sqlx.DB
+
+	for i := 0; i < 10; i++ {
+		db, err = sqlx.Connect("postgres", DSN)
+		if err == nil {
+			break
+		}
+
+		log.Println("Waiting for database...")
+		time.Sleep(2 * time.Second)
+	}
+
+	if err != nil {
+		log.Fatalf("Could not connect to database: %v", err)
+	}
+
+	fmt.Println("database connected successfully")
+	fmt.Println("database connected successfully")
+
+	//dependency injection
+	UserRepo := repository.NewUserRepository(db)
+	UserService := services.NewAuthService(UserRepo)
+	UserHandler := handlers.NewAuthHandler(UserService)
+
 	r := gin.Default()
-	r.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "repLog is alive",
-		})
-	})
+
+	auth := r.Group("/auth")
+	{
+		auth.POST("/register", UserHandler.RegisterUser)
+	}
 
 	fmt.Println("server is starting at port 8080....")
 	r.Run(":8080")
