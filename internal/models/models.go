3f3f83deeb7b38bd3ee8c8e4b8476318821c382c
deeep8250 <deepmondal825005@gmail.com>
2026-03-17 18:19:32 +0530

    added validation in login and register

--- internal/models/models.go
+++ internal/models/models.go
@@ -10,13 +10,13 @@ type User struct {
 }
 
 type RegisterRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6"`
 }
 
 type LoginRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6"`
 }
 
 type AuthResponse struct {
